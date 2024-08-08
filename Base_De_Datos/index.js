95d4526efcfcdcec800260a7d5ecae55c239b196
BenjaPicca <48386751@est.ort.edu.ar>
2024-08-08 12:15:14 -0300

    feat(Base_de_datos):Hice update delete y empece con req.body

--- Base_De_Datos/index.js
+++ Base_De_Datos/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {client} from "./.dbconfig.js"
+import { client } from "./.dbconfig.js"
 // Creamos el servidor de Express con la configuración estándar básica
 const app = express();
 
@@ -15,13 +15,40 @@ app.get("/chau", (req, res) => {
 });
 
 app.post("/insertar", async (req, res) => {
-    await client.query('INSERT INTO public."Usuario" (Nombre, Apellido, NombreUsuario, Mail, Contraseña) VALUES ($1, $2, $3, $4, $5)',
-        ["Benjamin", "Piccagli", "BenjaPicca", "benjapiccagli@gmail.com", "labruna9"]);
+    const {
+        Mail,
+        Nombre,
+        Apellido,
+        NombreUsuario,
+        Contraseña
+    } = req.body;
+
+    await client.query('INSERT INTO public."Usuario" ("Nombre", "Apellido", "NombreUsuario", "Mail", "Contraseña") VALUES ($1, $2, $3, $4, $5)',
+        [Nombre, Apellido, NombreUsuario, Mail, Contraseña]);
     res.send("Se ha insertado Correctamente")
 })
 
-app.get("/prueba",async(req,res)=>{
-    await client.query('SELECT Mail, Nombre, NombreUsuario FROM public."Usuario" WHERE ID=$1',[1])
+app.get("/prueba", async (req, res) => {
+    const Mail= req.body.Mail
+    const { rows } = await client.query('SELECT "Nombre", "Mail", "NombreUsuario" FROM public."Usuario" WHERE "Mail"=$1', [Mail])
+    res.json(rows[0])
+})
+
+app.delete("/delUsuario",async(req,res)=>{
+    const Mail=req.body.Mail
+    await client.query('DELETE * FROM public."Usuario" WHERE "Mail"=$1'),[Mail]
+})
+
+app.put("/Update", async(req,res)=>{
+    const{
+        Mail,
+        Nombre,
+        Apellido,
+        NombreUsuario,
+        Contraseña
+    } = req.body;
+    const {rows} = await client.query('UPDATE public."Usuario" SET "Nombre" =$1, "Apellido"=$2, "NombreUsuario"=$3, "Contraseña"=$4 WHERE "Mail"=$5 ',[Nombre,Apellido,NombreUsuario, Contraseña,Mail])
+    res.json(rows)
 })
 app.listen(3000, () => {
     console.log("Example app listening on port 3000!");
