bac7d386220e85c562d1fa3625dd3bb947ecd2f1
BenjaPicca <48386751@est.ort.edu.ar>
2024-08-08 13:50:21 -0300

    fix(Base_De_Datos):Arregle delte no anda mail.req.body

--- Base_De_Datos/index.js
+++ Base_De_Datos/index.js
@@ -29,25 +29,26 @@ app.post("/insertar", async (req, res) => {
 })
 
 app.get("/prueba", async (req, res) => {
-    const Mail= req.body.Mail
+    const Mail = req.body.Mail;
     const { rows } = await client.query('SELECT "Nombre", "Mail", "NombreUsuario" FROM public."Usuario" WHERE "Mail"=$1', [Mail])
     res.json(rows[0])
 })
 
-app.delete("/delUsuario",async(req,res)=>{
-    const Mail=req.body.Mail
-    await client.query('DELETE * FROM public."Usuario" WHERE "Mail"=$1'),[Mail]
+app.delete("/delUsuario", async (req, res) => {
+    const Mail = req.body.Mail;
+    await client.query('DELETE * FROM public."Usuario" WHERE "Mail"=$1', [Mail])
+    res.send("Se ha eliminado", { Mail })
 })
 
-app.put("/Update", async(req,res)=>{
-    const{
-        Mail,
+app.put("/Update", async (req, res) => {
+    const {
+        mail,
         Nombre,
         Apellido,
         NombreUsuario,
         Contraseña
     } = req.body;
-    const {rows} = await client.query('UPDATE public."Usuario" SET "Nombre" =$1, "Apellido"=$2, "NombreUsuario"=$3, "Contraseña"=$4 WHERE "Mail"=$5 ',[Nombre,Apellido,NombreUsuario, Contraseña,Mail])
+    const { rows } = await client.query('UPDATE public."Usuario" SET "Nombre" =$1, "Apellido"=$2, "NombreUsuario"=$3, "Contraseña"=$4 WHERE "Mail"=$5 ', [Nombre, Apellido, NombreUsuario, Contraseña, mail])
     res.json(rows)
 })
 app.listen(3000, () => {
