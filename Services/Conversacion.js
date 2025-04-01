75ec68fbe2068a03d113d87f696deba0ddef553b
BenjaPicca <48386751@est.ort.edu.ar>
2025-04-01 15:18:05 -0300

    Feat(Conv.Services)Services pasados

--- Services/Conversacion.js
+++ Services/Conversacion.js
@@ -44,3 +44,131 @@ const CreateFeed= async(mailusuario,feedback)=>{
         return res.status(500)
     }
 }
+const CreateVideo= async(mailusuario)=>{
+    const pool= new Pool(config);
+    await pool.connect();
+
+    try{
+        const result= await pool.query
+        (
+            `INSERT INTO public."Conversación"("Video_Inicial","Fecha_Conversación","Mail_Usuario",estado) VALUES ($1,$2,$3,'pendiente') RETURNING "ID"`
+            [url, new Date(), mailusuario])
+            console.log(result);
+            const ID=result.rows[0].ID;
+        
+        const body = {
+                        id: ID,
+                        url: url
+                    } //Aca va el node-fetch
+                    fetch('http://127.0.0.1:8000/translate', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(body),
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data);
+
+                            if (data.message === "received") {
+                                console.log("Video enviado:", data);
+                            } else {
+                                // Muestra un mensaje de error
+                                console.log("Error: " + data.message);
+                            }
+                            res.status(200).json({ message: 'Video agregado.',ID})
+                        })
+
+                    
+                } catch (err) {
+                    console.error(err);
+                    return res.status(500).json({ message: 'Error al agregegar video' });
+                }
+}
+
+const deleteConversaciónById= async(id)=>{
+    const pool= new Pool(config);
+    await pool.connect();
+
+    try{
+        await pool.query(
+            `DELETE FROM public."Conversación" WHERE "ID"=$1`, [id]
+        )
+        await pool.end();
+        return res.status(200).json({ message: 'Se ha eliminado correctamente' })
+    }
+    catch (err) {
+        await pool.end();
+        return res.status(500).json({ message: 'Error al eliminar usuario' })
+    }
+}
+
+const updateFeed = async(id)=>{
+    const pool= new Pool(config);
+    await pool.connect();
+
+    try{
+        await pool.query(`
+           UPDATE public."Conversación" SET "Feedback"=$1, "Fecha_Conversación"=$3 WHERE "ID"=$2
+            `[Feedback, id, new Date()])
+
+        await pool.end();
+        return res.status(200).json({message:'Se ha actualizado la tabla correctamente'})
+    }
+    catch(err){
+        await pool.end();
+        return res.status(500).json({message:'Error al actualizar FeedBack'})
+    }
+}
+const textoEntregado= async(id,translation)=>{
+    const pool= new Pool(config);
+    await pool.connect();
+
+    try{
+        if (translation === "Error") {
+            return res.status(200).json({ message: 'Fail Translator' })
+        }
+        await pool.query(`
+           UPDATE public."Conversación" 
+        SET "Texto_Devuelto" = $1, "Fecha_Conversación" = $3, estado = 'entregado'
+         WHERE "ID" = $2 `[translation, id, new Date()])
+         await pool.end();
+         return res.status(200).json({ message: 'Texto entregado' })
+    }
+    catch(err){
+        await pool.end();
+        return res.status(500).json({ message: err.message })
+    }
+}
+
+const getTexto= async(id)=>{
+    const pool=new Pool(config);
+    await pool.connect();
+
+    try{
+        const rows= await pool.query(`
+            SELECT "Texto_Devuelto" FROM public."Conversación" WHERE "ID"=$1`[id])
+            if(rows.length<1){
+                await pool.end();
+                return res.status(404).json({message:"No hay ningun texto"})
+            }
+             await pool.end();
+            return res.status(200).json(rows[0])
+    }
+    catch(err){
+        await pool.end();
+        return res.status(500).json({message:'No se pudo encontrar el texto'})
+    }
+}
+
+export default{
+ SelectFeedById,
+ CreateFeed,
+ CreateVideo,
+ deleteConversaciónById,
+ updateFeed,
+ textoEntregado,
+ getTexto
+
+}
